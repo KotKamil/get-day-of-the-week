7bc59183a05e7415505dfbb2a3ec1a1aaa2cf00e
KotKamil <62114754+KotKamil@users.noreply.github.com>
2023-02-06 17:05:28 +0100

    Added getMonthLength function

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 module.exports = getDayOfTheWeek = (date) => {
     return new Date(date).getDay();
 }
+
+module.exports = getMonthLength = (year, month) => {
+    return new Date(year, month, 0).getDate();
+}
